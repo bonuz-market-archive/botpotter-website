e764e75441104d548339a035979aee591a9c1fd5
Muzzamil Mushtaq <muzzamilmushtaq1@gmail.com>
2022-09-13 13:15:38 +0500

    change about to docs route

--- src/layouts/main/MenuConfig.tsx
+++ src/layouts/main/MenuConfig.tsx
@@ -3,45 +3,45 @@ import { PATH_PAGE } from '../../routes/paths';
 // ----------------------------------------------------------------------
 
 const menuConfig = [
-  // {
-  //   title: 'Home',
-  //   path: '/',
-  //   // children: [
-  //   //   {
-  //   //     items: [
-  //   //       {
-  //   //         title: 'Phrase',
-  //   //         path: PATH_PAGE.phrase,
-  //   //       },
-  //   //       {
-  //   //         title: 'Who Am I',
-  //   //         path: PATH_PAGE.whoAmI,
-  //   //       },
-  //   //       {
-  //   //         title: 'Topic',
-  //   //         path: PATH_PAGE.topic,
-  //   //       }
-  //   //     ],
-  //   //   }
-  //   // ],
-  // },
+  {
+    title: 'Home',
+    path: '/',
+    // children: [
+    //   {
+    //     items: [
+    //       {
+    //         title: 'Phrase',
+    //         path: PATH_PAGE.phrase,
+    //       },
+    //       {
+    //         title: 'Who Am I',
+    //         path: PATH_PAGE.whoAmI,
+    //       },
+    //       {
+    //         title: 'Topic',
+    //         path: PATH_PAGE.topic,
+    //       }
+    //     ],
+    //   }
+    // ],
+  },
   {
     title: 'Browse',
     path: PATH_PAGE.browse,
   },
 
-  {
-    title: 'Create Bot',
-    path: PATH_PAGE.createBot,
-  },
+  // {
+  //   title: 'Create Bot',
+  //   path: PATH_PAGE.createBot,
+  // },
 
   {
     title: 'Community',
     path: PATH_PAGE.community,
   },
   {
-    title: 'About',
-    path: PATH_PAGE.about,
+    title: 'Docs',
+    path: PATH_PAGE.docs,
   }
 ];
 
