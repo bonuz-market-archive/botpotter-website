0ca1f77cb5c10151bc0efc54ee7bed389299f831
Muzzamil Mushtaq <muzzamilmushtaq1@gmail.com>
2022-09-15 13:15:17 +0500

    documentation: add markdown styles

--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -1,11 +1,34 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-//@ts-ignore
+//@ts-nocheck
+
+import { Box } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import rdmd from '@readme/markdown';
 
+// ----------------------------------------------------------------------
+const MarkDownStyle = styled(Box)(({ theme, }) => ({
+  '& .heading': {
+    fontFamily: 'Flame',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: '32px',
+    lineHeight: '38px',
+    color: '#000000',
+  },
+  '& li': {
+    marginLeft: theme.spacing(2.5),
+  },
+}));
+
+// ----------------------------------------------------------------------
 export interface Props {
   body?: string;
 }
 
 export default function Markdown({ body, }: Props) {
-  return <div className='markdown-body'>{rdmd(body)}</div>;
+  return (
+    <MarkDownStyle>
+      <div className='markdown-body'>{rdmd(body)}</div>
+    </MarkDownStyle>
+  );
 }
